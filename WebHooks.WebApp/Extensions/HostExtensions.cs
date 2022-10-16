1e48ba20b4c4f07531b231ee6efa6ea42c1d0087
Alessio Gogna <5177307+alecsg77@users.noreply.github.com>
2022-10-16 12:39:29 +0200

    Create/Migrate database at startup

---
+++ WebHooks.WebApp/Extensions/HostExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostExtensions
+    {
+        /// <summary>  
+        /// Migrates the database.  
+        /// </summary>  
+        /// <typeparam name="T"></typeparam>  
+        /// <param name="host">The host.</param>  
+        /// <returns>IHost.</returns>  
+        public static IHost CreateDatabase<T>(this IHost host) where T : DbContext
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var db = services.GetRequiredService<T>();
+                db.Database.Migrate();
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "Database Creation/Migrations failed!");
+                throw new InvalidOperationException("Database Creation/Migrations failed", ex);
+            }
+
+            return host;
+        }
+    }
+}
--- WebHooks.WebApp/Program.cs
+++ WebHooks.WebApp/Program.cs
@@ -39,4 +39,6 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+app.CreateDatabase<ApplicationDbContext>();
+
 app.Run();
