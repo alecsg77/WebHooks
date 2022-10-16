f58d82979ac925d79440ad6fecf200953bc30396
Alessio Gogna <5177307+alecsg77@users.noreply.github.com>
2022-10-16 18:43:02 +0200

    Add Swashbuckle

--- WebHooks.WebApp/Program.cs
+++ WebHooks.WebApp/Program.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.OpenApi.Models;
 using WebHooks.WebApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,12 +16,38 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
+builder.Services.AddSwaggerGen(
+options =>
+{
+    options.AddSecurityDefinition("identity", new OpenApiSecurityScheme()
+    {
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Cookie,
+        Name = CookieAuthenticationDefaults.CookiePrefix + IdentityConstants.ApplicationScheme
+    });
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme{
+                    Reference = new OpenApiReference{
+                        Id = "identity",
+                        Type = ReferenceType.SecurityScheme
+                    }
+                },
+                new List<string>()
+            }
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
